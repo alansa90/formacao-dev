fc2f2a6041e00c3be2b2ad90c313dae6533e0ab0
Alan Sa <alansa@MacBook-Air-de-Alan.local>
2024-11-20 18:09:30 -0300

    chore: reconfigure git

---
+++ apps/frontend/src/components/template/LogoGrande.tsx
@@ -0,0 +1,28 @@
+import { Righteous } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const fonte =  Righteous({
+  subsets: ['latin'],
+  weight: '400',
+})
+
+export default function LogoGrande() {
+  return (
+    <div>
+      <Link
+        className={`
+      flex flex-col items-center
+      gap-2
+      ${fonte.className}
+      `}
+        href="/"
+      >
+        <Image src="/logo.svg" width={100} height={100} alt="Logo" />
+        <h1 className="text-5xl">
+          CONVIT<span className="text-blue-500">3</span> DIGITAL
+        </h1>
+      </Link>
+    </div>
+  );
+}
